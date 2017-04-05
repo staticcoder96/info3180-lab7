de263c3aa65d4dddaf35e150629f1731b544f302
Stephanie Ramsay <stephanie.ramsay@mymona.uwi.edu>
2017-04-05 03:19:09 +0000

    Exercise3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,7 @@
 // Your JavaScript Code here
+var app = angular.module('myApp',[]);
+app.controller('thumbsnails', function($scope,$http){
+    $http.post("/api/thumbnails").then(function(response){
+        $scope.urls = response.data.thumbsnails;
+    });
+});
